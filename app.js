bb3b4e777f13383816b74529b4de7dc93e791955
CuongNB <cuongnb3698@gmail.com>
2023-03-07 23:54:50 +0700

    Initial

---
+++ app.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    $("#canvas").click(function () {
+        $("#ting").trigger('play');
+        $(".gift").hide('fade', 1200);
+        $(".page1").hide('fade', 1500);
+        setTimeout(() => {
+            $('.page2').show('fade', 300);
+            $('#video').trigger('play');
+            $('#audio').trigger('play');
+
+            setText()
+        }, 1200);
+    });
+});
+
+
+function setText() {
+    let time = 3000;
+
+    const textVN = `Vào ngày Quốc tế phụ nữ chúc mọi thứ tốt đẹp nhất sẽ đến với bạn Chúc bạn một 8/3 hạnh phúc!`;
+    const textEN = `♥ On women’s day what can I wish for, but the very best for you! Happy Women’s Day!`;
+
+    const arrStr = textVN.split(' ');
+    const arrStr2 = textEN.split(' ');
+    arrStr.forEach(str => {
+        time += 200;
+        $('#box-text').append('<span>' + str + '&nbsp;</span>');
+    });
+    setTimeout(() => {
+        time = 20000;
+        $('#box-text').hide('slow');
+
+        setTimeout(() => {
+            time += 200;
+            $('#box-text').empty();
+            $('#box-text').show('slow');
+            arrStr2.forEach(str => {
+                $('#box-text').append('<span>' + str + '&nbsp;</span>');
+            });
+        }, 1700);
+
+        setTimeout(() => {
+            $(".gift").show('fade', 1200);
+            $(".page1").show('fade', 1500);
+            $('.page2').hide('fade', 300);
+        }, time);
+
+    }, time);
+}
