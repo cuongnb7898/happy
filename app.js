74c3dfc26570456c12b23b7fced50143b0f27bde
CuongNB <cuongnb3698@gmail.com>
2023-03-08 00:51:09 +0700

    fix

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ $(document).ready(function () {
         setTimeout(() => {
             $('.page2').show('fade');
             $('#video').trigger('play');
-            $('#audio').trigger('play');
-
             setText()
         }, 1500);
     });
@@ -15,6 +13,7 @@ $(document).ready(function () {
 
 
 function setText() {
+    $('#audio')[0].play();
     let time = 3000;
 
     const textVN = `Vào ngày Quốc tế phụ nữ chúc mọi thứ tốt đẹp nhất sẽ đến với bạn Chúc bạn một 8/3 hạnh phúc!`;
