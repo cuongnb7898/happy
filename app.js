179bab78fc699544afe2e5fedd2d7b24d26f2cb8
CuongNB <cuongnb3698@gmail.com>
2023-03-08 08:28:47 +0700

    fx

--- app.js
+++ app.js
@@ -24,21 +24,21 @@ $(document).ready(function () {
 
 function setText() {
     $('#audio').trigger('play');
-
-
     let time = 3000;
     $('#box-text').empty();
     const textVN = `Vào ngày Quốc tế phụ nữ chúc mọi thứ tốt đẹp nhất sẽ đến với bạn Chúc bạn một 8/3 hạnh phúc!`;
     const textEN = `♥ On women’s day what can I wish for, but the very best for you! Happy Women’s Day!`;
-
+    const strEnd = 'Vett & VTech'
     const arrStr = textVN.split(' ');
     const arrStr2 = textEN.split(' ');
+    const arrStr3 = strEnd.split(' ');
+
     arrStr.forEach(str => {
         time += 200;
         $('#box-text').append('<span>' + str + '&nbsp;</span>');
     });
     setTimeout(() => {
-        time = 20000;
+        time = 12000;
         $('#box-text').hide('slow');
 
         setTimeout(() => {
@@ -48,13 +48,22 @@ function setText() {
             arrStr2.forEach(str => {
                 $('#box-text').append('<span>' + str + '&nbsp;</span>');
             });
-        }, 1700);
+        }, 1000);
+
+        setTimeout(() => {
+            time += 200;
+            $('#box-text').empty();
+            $('#box-text').show('slow');
+            arrStr3.forEach(str => {
+                $('#box-text').append('<span>' + str + '&nbsp;</span>');
+            });
+        }, 26000);
 
         setTimeout(() => {
             $(".gift").show('fade', 1200);
             $(".page1").show('fade', 1500);
             $('.page2').hide('fade', 300);
-        }, time);
+        }, 30000);
 
     }, time);
 }
