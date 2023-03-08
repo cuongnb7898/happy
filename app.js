1fb8f470704ba09527a2d84a5a4d754cb20e6a95
CuongNB <cuongnb3698@gmail.com>
2023-03-08 08:33:42 +0700

    fix

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ $(document).ready(function () {
         $(".gift").hide('fade', 1200);
         $(".page1").hide('fade', 1500);
         setTimeout(() => {
-            $('.page2').show('fade');
+            $('.page2').show();
             $('#video').trigger('play');
             $('#video').trigger('muted');
 
             setText()
-        }, 1500);
+        }, 2300);
     });
 
 
@@ -24,7 +24,7 @@ $(document).ready(function () {
 
 function setText() {
     $('#audio').trigger('play');
-    let time = 3000;
+    let time = 2100;
     $('#box-text').empty();
     const textVN = `Vào ngày Quốc tế phụ nữ chúc mọi thứ tốt đẹp nhất sẽ đến với bạn Chúc bạn một 8/3 hạnh phúc!`;
     const textEN = `♥ On women’s day what can I wish for, but the very best for you! Happy Women’s Day!`;
@@ -33,37 +33,39 @@ function setText() {
     const arrStr2 = textEN.split(' ');
     const arrStr3 = strEnd.split(' ');
 
-    arrStr.forEach(str => {
-        time += 200;
-        $('#box-text').append('<span>' + str + '&nbsp;</span>');
-    });
     setTimeout(() => {
-        time = 12000;
-        $('#box-text').hide('slow');
-
-        setTimeout(() => {
+        arrStr.forEach(str => {
             time += 200;
-            $('#box-text').empty();
-            $('#box-text').show('slow');
-            arrStr2.forEach(str => {
-                $('#box-text').append('<span>' + str + '&nbsp;</span>');
-            });
-        }, 1000);
-
+            $('#box-text').append('<span>' + str + '&nbsp;</span>');
+        });
         setTimeout(() => {
-            time += 200;
-            $('#box-text').empty();
-            $('#box-text').show('slow');
-            arrStr3.forEach(str => {
-                $('#box-text').append('<span>' + str + '&nbsp;</span>');
-            });
-        }, 26000);
+            time = 12000;
+            $('#box-text').hide('slow');
 
-        setTimeout(() => {
-            $(".gift").show('fade', 1200);
-            $(".page1").show('fade', 1500);
-            $('.page2').hide('fade', 300);
-        }, 30000);
+            setTimeout(() => {
+                time += 200;
+                $('#box-text').empty();
+                $('#box-text').show('slow');
+                arrStr2.forEach(str => {
+                    $('#box-text').append('<span>' + str + '&nbsp;</span>');
+                });
+            }, 600);
 
-    }, time);
+            setTimeout(() => {
+                time += 200;
+                $('#box-text').empty();
+                $('#box-text').show('slow');
+                arrStr3.forEach(str => {
+                    $('#box-text').append('<span>' + str + '&nbsp;</span>');
+                });
+            }, 26000);
+
+            setTimeout(() => {
+                $(".gift").show('fade', 1200);
+                $(".page1").show('fade', 1500);
+                $('.page2').hide('fade', 300);
+            }, 30000);
+
+        }, time);
+    }, 500);
 }
